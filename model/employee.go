eddd7129e53d5eaac2d43ed603965db2f548312a
sidz111 <sssurwade2212@gmail.com>
2026-02-27 17:31:47 +0530

    worked on models

--- model/employee.go
+++ model/employee.go
@@ -1,7 +1,8 @@
 package model
 
 type Employee struct {
-	ID     int
-	Name   string
-	Salary float64
+	ID      int     `gorm:"primaryKey; autoincrement" json:"id"`
+	Name    string  `json:"name"`
+	Salary  float64 `json:"salary"`
+	Project Project `gorm:"constraint:OnDelete:CASCADE" json:"project"`
 }
--- model/project.go
+++ model/project.go
@@ -1,6 +1,12 @@
 package model
 
+import (
+	"time"
+)
+
 type Project struct {
-	ID    int
-	Title string
+	ID         int       `gorm:"primaryKey;autoincrement" json:"id"`
+	Title      string    `json:"title"`
+	AssignDate time.Time `json:"assignDate"`
+	EmployeeID int       `json:"employeeId"`
 }
