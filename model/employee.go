7b3660e0e0e537692fb421be824a3d4beb909f76
sidz111 <sssurwade2212@gmail.com>
2026-03-01 02:09:27 +0530

    update mapping

--- model/employee.go
+++ model/employee.go
@@ -1,8 +1,8 @@
 package model
 
 type Employee struct {
-	ID      int     `gorm:"primaryKey; autoincrement" json:"id"`
-	Name    string  `json:"name"`
-	Salary  float64 `json:"salary"`
-	Project Project `gorm:"constraint:OnDelete:CASCADE" json:"project"`
+	ID      int      `gorm:"primaryKey; autoincrement" json:"id"`
+	Name    string   `json:"name"`
+	Salary  float64  `json:"salary"`
+	Project *Project `gorm:"foreignKey:EmployeeID;constraint:OnDelete:CASCADE" json:"project"`
 }
